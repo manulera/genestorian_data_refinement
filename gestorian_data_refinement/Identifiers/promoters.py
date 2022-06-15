e2eea62779c8377cb491a3c52a26106f7815190b
Anamika Yadav <anamika310.yadav@gmail.com>
2022-06-15 16:15:06 +0530

    delete duplicates

--- gestorian_data_refinement/Identifiers/promoters.py
+++
@@ -1,1 +0,0 @@
-promoters = [r'p?nmt\d*',r'p?adh\d*',r'prep\d*']
--- gestorian_data_refinement/__init__.py
+++
@@ -1,3 +0,0 @@
-from gestorian_data_refinement.pattern_identify import (Analyse, count_most_common_consecutive_N_characters)
-from gestorian_data_refinement.converge import converge
-__all__ = ['Analyse', 'count_most_common_consecutive_N_characters']
