22478bb03dc6232d0eab6b58f9e54a1c166c19b4
Anamika Yadav <anamika310.yadav@gmail.com>
2022-06-17 20:49:56 +0530

    JSON file of identifiers and occurences

--- genestorian_data_refinement/__init__.py
+++ genestorian_data_refinement/__init__.py
@@ -1,3 +1,4 @@
 from genestorian_data_refinement.pattern_identify import (Analyse, count_most_common_consecutive_N_characters, find)
 from genestorian_data_refinement.converge import converge
+from genestorian_data_refinement.matched_pattern import Matched_Patterns
 __all__ = ['Analyse', 'count_most_common_consecutive_N_characters', 'find']
