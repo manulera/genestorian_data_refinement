d307688208bf7fb988d7de74a46066a6083f4e85
Anamika Yadav <anamika310.yadav@gmail.com>
2022-06-23 18:10:32 +0530

    genestorian_module

---
+++ genestorian_module/genestorian_module/__init__.py
@@ -0,0 +1,7 @@
+from genestorian_data_refinement.matched_pattern import Matched_Patterns
+from genestorian_data_refinement.converge import converge
+from genestorian_data_refinement.pattern_identify import (
+    Analyse, count_most_common_consecutive_N_characters, find)
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
---
+++ genestorian_module/setup.py
@@ -0,0 +1,2 @@
+from setuptools import setup, find_packages
+setup(name='genestorian_data_refinement', packages=find_packages())
