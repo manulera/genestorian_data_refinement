03b6cb79fb3d323228cf165f326b4e61930e6d0f
Anamika Yadav <anamika310.yadav@gmail.com>
2022-06-24 17:56:35 +0530

    gene_id.toml improved

--- get_data/convert_gene2toml.py
+++ get_data/convert_gene2toml.py
@@ -1,36 +1,25 @@
 # %%
 import toml
+import re
 
 toml_dict = {'gene': dict()}
 
 with open('../data/gene_IDs_names.tsv') as ins:
     for line in ins:
         ls = line.strip().split('\t')
-        systematic_gene_id = ls[:1]
-        if len(ls[1:2]) == 0:
-            if len(ls[2:3]) == 0:
-                toml_dict['gene'][systematic_gene_id[0]] = {
-                    'ref': systematic_gene_id[0],
-                }
-            else:
-                synonyms = ls[2:3]
-                toml_dict['gene'][systematic_gene_id[0]] = {
-                    'ref': systematic_gene_id[0],
-                    'synonyms': synonyms
-                }
-        else:
-            main_gene_name = ls[1:2]
-            if len(ls[2:3]) == 0:
-                toml_dict['gene'][systematic_gene_id[0]] = {
-                    'ref': systematic_gene_id[0],
-                    'name': main_gene_name[0]
-                }
-            else:
-                synonyms = ls[2:3]
-                toml_dict['gene'][systematic_gene_id[0]] = {
-                    'ref': systematic_gene_id[0],
-                    'name': main_gene_name[0],
-                    'synonyms': synonyms
-                }
+        systematic_gene_id = ls[0]
+        toml_dict['gene'][systematic_gene_id] = {
+            'ref': systematic_gene_id,
+        }
+
+        if len(ls) > 1:
+            toml_dict['gene'][systematic_gene_id]['name'] = ls[1]
+
+        if len(ls) > 2:
+            synonyms = re.split(",", ls[2])
+            toml_dict['gene'][systematic_gene_id]['synonyms'] = synonyms
 with open('../data/gene_IDs.toml', "w") as toml_file:
     toml.dump(toml_dict, toml_file)
+
+
+# %%
