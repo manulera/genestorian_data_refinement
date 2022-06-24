b5cd18b6360a0ea1f00c91b07fca65d34ae223f9
Anamika Yadav <anamika310.yadav@gmail.com>
2022-06-24 16:51:56 +0530

    convert_gene2toml

---
+++ genestorian_module/genestorian_module/generic_func.py
@@ -0,0 +1,3 @@
+class replace_allele_features():
+    def __init__():
+        pass
--- get_data/convert_alleles2toml.py
+++ get_data/convert_alleles2toml.py
@@ -1,38 +1,21 @@
 # %%
 import toml
 
-toml_dict = {'gene': dict()}
+# %%
+toml_dict = {'allele': dict()}
 
-with open('../data/gene_IDs_names.tsv') as ins:
+with open('../data/alleles_pombemine.tsv') as ins:
     for line in ins:
         ls = line.strip().split('\t')
-        systematic_gene_id = ls[:1]
-        if len(ls[1:2]) == 0:
-            if len(ls[2:3]) == 0:
-                toml_dict['gene'][systematic_gene_id[0]] = {
-                    'ref': systematic_gene_id[0],
-                }
-            else:
-                synonyms = ls[2:3]
-                toml_dict['gene'][systematic_gene_id[0]] = {
-                    'ref': systematic_gene_id[0],
-                    'synonyms': synonyms
-                }
-        else:
-            main_gene_name = ls[1:2]
-            if len(ls[2:3]) == 0:
-                toml_dict['gene'][systematic_gene_id[0]] = {
-                    'ref': systematic_gene_id[0],
-                    'name': main_gene_name[0]
-                }
-            else:
-                synonyms = ls[2:3]
-                toml_dict['gene'][systematic_gene_id[0]] = {
-                    'ref': systematic_gene_id[0],
-                    'name': main_gene_name[0],
-                    'synonyms': synonyms
-                }
-with open('../data/gene_IDs.toml', "w") as toml_file:
-    toml.dump(toml_dict, toml_file)
+        # For now we skip alleles with delta in them. In the future we may
+        # have to consider some rec12 / rec8 allele was mentioned in some
+        # issue that had an equivalent name
+        if 'delta' not in ls[2]:
+            systematic_id, main_gene_name, allele_name = ls[:3]
+            toml_dict['allele'][allele_name] = {
+                'name': allele_name,
+                'ref': systematic_id
+            }
 
-# %%
+with open('../data/alleles.toml', "w") as toml_file:
+    toml.dump(toml_dict, toml_file)
