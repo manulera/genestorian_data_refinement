43d38488a2932c89eddd4958db732023b999f323
Anamika Yadav <anamika310.yadav@gmail.com>
2022-06-27 20:11:05 +0530

    WIP: genaralised func

--- Lab_strains/dey_lab/generalised_function.py
+++ Lab_strains/dey_lab/generalised_function.py
@@ -1,6 +1,7 @@
 # %%
 import re
 import toml
+import json
 
 
 def feature_name(toml_file):
@@ -9,113 +10,50 @@ def feature_name(toml_file):
     return feature[0]
 
 
-def gene_dict(toml_file):
-    gene_dict = {}
+def feature_dict(toml_file):
+    feature_dict = {}
     f = toml.load(toml_file)
-    systematic_id = f['gene'].keys()
-    for id in systematic_id:
-        if 'name' in f['gene'][id].keys():
-            names = f['gene'][id]['name']
-            gene_dict.update({names: id})
-        if 'synonyms' in f['gene'][id].keys():
-            synonym = f['gene'][id]['synonyms']
+    toml_key_name = feature_name(toml_file)
+    toml_keys = f[toml_key_name].keys()
+    for toml_key in toml_keys:
+        if 'name' in f[toml_key_name][toml_key].keys():
+            names = f[toml_key_name][toml_key]['name']
+            feature_dict.update({names: toml_key})
+        if 'synonyms' in f[toml_key_name][toml_key].keys():
+            synonym = f[toml_key_name][toml_key]['synonyms']
             for sy in synonym:
-                gene = re.match(r'[a-z]{3}\d+', sy)
-                systematic_id = re.match(r'SP.+\.\d+c?', sy)
-                gene_dict[gene] = id
-                gene_dict[gene] = systematic_id
-    return gene_dict
-
-
-def allele_dict(toml_file):
-    allele_dict = {}
-    f = toml.load(toml_file)
-    allele_list_toml = f['allele'].keys()
-    #allele_list_toml.sort(key=len, reverse=True)
-
-    for alleles in allele_list_toml:
-        allele_ref_id = f['allele'][alleles]['ref']
-        if allele_ref_id not in allele_dict.keys():
-            allele_dict.update({alleles: allele_ref_id})
-
-    #allele_dict.keys().sort(key=len, reverse=True)
-    return allele_dict
-
-
-def other_dict(toml_file, replace_word):
-    feature = replace_word.lower()
-    other_feature_dict = {}
-    f = toml.load(toml_file)
-    feature_list_toml = f[feature].keys()
-    other_feature_dict.update({feature: [feature_list_toml]})
-    return other_feature_dict
-
-
-def is_allele(allele_dict, genotype, replace_word):
-    alleles = []
-    for allele in genotype:
-        for a in allele_dict.keys():
-            if a in allele.lower():
-                allele = allele.replace(a.lower(), replace_word)
-                alleles.append(allele)
-
-    return alleles
-
-
-def is_gene(gene_dict, genotype, replace_word):
-    genes = []
-    for allele in genotype:
-        for name in re.findall(r'[a-z]{3}\d+', allele):
-            if name.lower() in gene_dict.keys():
-                allele = allele.replace(name, replace_word)
-                genes.append(allele)
-    return genes
-
-
-def is_other(genotype, other_feature_dict, replace_word):
-    genotype = []
-    feature = replace_word.lower()
-    for alleles in genotype:
-        for keys in other_feature_dict.keys():
-            print(keys)
-            if keys in alleles:
-                alleles = alleles.replace(keys, replace_word)
-        genotype.append(alleles)
-    return genotype
+                feature_dict[sy] = toml_key
+    return feature_dict
 
 
 def replace_allele_features(toml_file, genotype, replace_word):
-    feature_to_replace = feature_name(toml_file)
-    if feature_to_replace == 'gene':
-        gene_DICTIONARY = gene_dict(toml_file)
-        replaced_word = is_gene(gene_DICTIONARY, genotype, replace_word)
-    elif feature_to_replace == 'allele':
-        alleledict = allele_dict(toml_file)
-        replaced_word = is_allele(alleledict, genotype, replace_word)
-    else:
-        feature_dict = other_dict(toml_file, replace_word)
-        replaced_word = is_other(genotype, feature_dict, replace_word)
-    return replaced_word
+    features = feature_dict(toml_file)
+    alleles = []
+    for allele in genotype:
+        for a in features.keys():
+            if a.lower() in allele.lower():
+                allele_r = allele.replace(a, replace_word)
+                alleles.append(allele_r)
+                break
+    if len(alleles) == 0:
+        alleles.append(genotype)
+    return alleles
 
 
-# %%
 genotype = ['cls1-36 ase1-GFP:KanMx6']
-genotype_a = replace_allele_features(
-    '../../data/alleles.toml', genotype, 'ALLELE')
-print(genotype_a)
-
-
-# %%
-
 
 genotype_g = replace_allele_features(
-    '../../data/gene_IDs.toml', genotype_a, 'GENE')
+    '../../data/gene_IDs.toml', genotype, 'GENES')
 print(genotype_g)
 
-# %%
-genotype_g = replace_allele_features(
-    '../../genestorian_module/genestorian_module/Identifiers/identifiers.toml', genotype_g, 'MARKER')
+genotype_t = replace_allele_features(
+    '../../allele_components/tags.toml', genotype_g, 'TAGS')
 
-# %%
-print(genotype_g)
+
+genotype_m = replace_allele_features(
+    '../../allele_components/markers.toml', genotype_t, 'MARKERS')
+
+print(genotype_m)
+
+
 # %%
