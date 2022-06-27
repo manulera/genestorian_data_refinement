42f3e074e45f919c15702950ec96fdef96d8f9ae
Anamika Yadav <anamika310.yadav@gmail.com>
2022-06-27 21:20:01 +0530

    generalised_func complete

--- Lab_strains/dey_lab/generalised_function.py
+++ Lab_strains/dey_lab/generalised_function.py
@@ -29,31 +29,42 @@ def feature_dict(toml_file):
 def replace_allele_features(toml_file, genotype, replace_word):
     features = feature_dict(toml_file)
     alleles = []
+    matches = []
     for allele in genotype:
         for a in features.keys():
             if a.lower() in allele.lower():
-                allele_r = allele.replace(a, replace_word)
-                alleles.append(allele_r)
-                break
+                if a != '':
+                    matches.append(a)
+    i = 0
+    while i < len(matches): 
+        a = matches[i]
+        allele = allele.replace(a, replace_word)
+        i +=1
+    alleles.append(allele)
+
     if len(alleles) == 0:
         alleles.append(genotype)
     return alleles
 
 
 genotype = ['cls1-36 ase1-GFP:KanMx6']
-
+genotype_a = replace_allele_features('../../data/alleles.toml', genotype, 'ALLELE')
+print(genotype_a)
 genotype_g = replace_allele_features(
-    '../../data/gene_IDs.toml', genotype, 'GENES')
+    '../../data/gene_IDs.toml', genotype_a, 'GENES')
 print(genotype_g)
 
+
+
+
+# %%
 genotype_t = replace_allele_features(
     '../../allele_components/tags.toml', genotype_g, 'TAGS')
 
-
+print(genotype_t)
 genotype_m = replace_allele_features(
     '../../allele_components/markers.toml', genotype_t, 'MARKERS')
 
 print(genotype_m)
-
 
 # %%
