bba86d322a3fac30a4932539b93b3cf0bc6a516a
Anamika Yadav <anamika310.yadav@gmail.com>
2022-06-27 13:13:40 +0530

    error : [genes] returns empty list

--- Lab_strains/dey_lab/generalised_function.py
+++ Lab_strains/dey_lab/generalised_function.py
@@ -1,7 +1,6 @@
 # %%
 import re
 import toml
-import re
 
 
 def feature_name(toml_file):
@@ -12,7 +11,7 @@ def feature_name(toml_file):
 
 def gene_dict(toml_file):
     gene_dict = {}
-    f = toml.load(toml)
+    f = toml.load(toml_file)
     systematic_id = f['gene'].keys()
     for id in systematic_id:
         if 'name' in f['gene'][id].keys():
@@ -20,7 +19,7 @@ def gene_dict(toml_file):
             gene_dict.update({names: id})
         if 'synonyms' in f['gene'][id].keys():
             synonym = f['gene'][id]['synonyms']
-            for sy in sy:
+            for sy in synonym:
                 gene = re.match(r'[a-z]{3}\d+', sy)
                 systematic_id = re.match(r'SP.+\.\d+c?', sy)
                 gene_dict[gene] = id
@@ -48,7 +47,7 @@ def other_dict(toml_file, replace_word):
     other_feature_dict = {}
     f = toml.load(toml_file)
 
-    feature_list_toml = f['feature'].keys()
+    feature_list_toml = f[feature].keys()
 
     other_feature_dict.update({feature: [feature_list_toml]})
     return other_feature_dict
@@ -59,19 +58,20 @@ def is_allele(allele_dict, genotype, replace_word):
     for allele in genotype:
         for a in allele_dict.keys():
             if a in allele.lower():
-                print(a)
                 allele = allele.replace(a.lower(), replace_word)
                 alleles.append(allele)
+
     return alleles
 
 
-def is_gene(allele, gene_dict, genotype, replace_word):
+def is_gene(gene_dict, genotype, replace_word):
     genes = []
     for allele in genotype:
         for name in re.findall(r'[a-z]{3}\d+', allele):
             if name.lower() in gene_dict.keys():
-                gene = gene.replace(name.lower(), replace_word)
-        genes.append(genes)
+                allele = allele.replace(name, replace_word)
+                genes.append(allele)
+    print(genes)
     return genes
 
 
@@ -89,8 +89,8 @@ def is_other(genotype, other_feature_dict, replace_word):
 def replace_allele_features(toml_file, genotype, replace_word):
     feature_to_replace = feature_name(toml_file)
     if feature_to_replace == 'gene':
-        genedict = gene_dict(toml_file)
-        replaced_word = is_gene(genedict, genotype, replace_word)
+        gene_DICTIONARY = gene_dict(toml_file)
+        replaced_word = is_gene(gene_DICTIONARY, genotype, replace_word)
     if feature_to_replace == 'allele':
         alleledict = allele_dict(toml_file)
         replaced_word = is_allele(alleledict, genotype, replace_word)
@@ -110,6 +110,8 @@ print(genotype_a)
 # %%
 genotype_g = replace_allele_features(
     '../../data/gene_IDs.toml', genotype_a, 'GENE')
+print(genotype_g)
+
 # %%
 # %%
 # %%
