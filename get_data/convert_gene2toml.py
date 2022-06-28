a58189d5a250a29bcbb1881ec2ff64a4a4232a63
Anamika Yadav <anamika310.yadav@gmail.com>
2022-06-28 11:17:27 +0530

    gene2toml correction on  the basis of feedback

--- get_data/convert_gene2toml.py
+++ get_data/convert_gene2toml.py
@@ -12,14 +12,12 @@ with open('../data/gene_IDs_names.tsv') as ins:
             'ref': systematic_gene_id,
         }
 
-        if len(ls) > 1:
-            if ls[1] != '':
-                toml_dict['gene'][systematic_gene_id]['name'] = ls[1]
+        if len(ls) > 1 and ls[1] != '':
+            toml_dict['gene'][systematic_gene_id]['name'] = ls[1]
 
-        if len(ls) > 2:
-            if ls[2] != '':
-                synonyms = re.split(",", ls[2])
-                toml_dict['gene'][systematic_gene_id]['synonyms'] = synonyms
+        if len(ls) > 2 and ls[2] != '':
+            synonyms = re.split(",", ls[2])
+            toml_dict['gene'][systematic_gene_id]['synonyms'] = synonyms
 with open('../data/gene_IDs.toml', "w") as toml_file:
     toml.dump(toml_dict, toml_file)
 
