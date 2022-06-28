09fcd89ba567a55aa942a40babe533a6028a3178
Anamika Yadav <anamika310.yadav@gmail.com>
2022-06-28 14:56:19 +0530

    comments added to generalised_function.py

--- Lab_strains/dey_lab/generalised_function.py
+++ Lab_strains/dey_lab/generalised_function.py
@@ -1,24 +1,24 @@
 # %%
-import re
 import toml
-import json
 
 
 def feature_dict(toml_file):
-    feature_dict = {}
-    f = toml.load(toml_file)
-    feature_name = list(f.keys())[0]
-
-    feature_keys = f[feature_name].keys()
-    for feature_key in feature_keys:
-        if 'name' in f[feature_name][feature_key].keys():
-            name = f[feature_name][feature_key]['name']
-            feature_dict[name] = feature_key
-        if 'synonyms' in f[feature_name][feature_key].keys():
-            synonyms = f[feature_name][feature_key]['synonyms']
+    #a dictionary in which the keys are name, synonyms and toml_keys and values are toml_keys 
+    synonyms_2toml_key_dict = {}
+    feature_type_dict = toml.load(toml_file)
+    feature_type_name = list(feature_type_dict.keys())[0]
+    #a dictionary in which the keys are toml keys and value is a dictionary of name, ref, sy
+    toml_key_2feature = feature_type_dict[feature_type_name]
+    for feature_key in toml_key_2feature:
+        if 'name' in toml_key_2feature[feature_key]:
+            name = toml_key_2feature[feature_key]['name']
+            synonyms_2toml_key_dict[name] = feature_key
+        if 'synonyms' in toml_key_2feature[feature_key]:
+            synonyms = toml_key_2feature[feature_key]['synonyms']
             for synonym in synonyms:
-                feature_dict[synonym] = feature_key
-    return feature_dict
+                synonyms_2toml_key_dict[synonym] = feature_key
+        synonyms_2toml_key_dict[feature_key] = feature_key
+    return synonyms_2toml_key_dict
 
 
 def replace_allele_features(toml_file, genotypes, replace_word):
@@ -30,19 +30,11 @@ def replace_allele_features(toml_file, genotypes, replace_word):
             if feature.lower() in genotype.lower():
                 if feature != '':
                     matches.append(feature)
-        for feature in features.values():
-            if feature.lower() in genotype.lower() and feature.lower() not in matches:
-                matches.append(feature)
         matches.sort(key=len, reverse=True)
-        i = 0
-        while i < len(matches):
-            a = matches[i]
-            genotype = genotype.replace(a, replace_word)
-            i += 1
+        for match in matches:
+            genotype = genotype.replace(match, replace_word)
         genotype_features_replaced.append(genotype)
 
-    if len(genotype_features_replaced) == 0:
-        return genotype
     return genotype_features_replaced
 
 
@@ -51,16 +43,18 @@ genotype_a = replace_allele_features(
     '../../data/alleles.toml', genotype, 'ALLELE')
 print(genotype_a)
 genotype_g = replace_allele_features(
-    '../../data/gene_IDs.toml', genotype_a, 'GENES')
+    '../../data/gene_IDs.toml', genotype_a, 'GENE')
 print(genotype_g)
 
 
 # %%
 genotype_t = replace_allele_features(
-    '../../allele_components/tags.toml', genotype_g, 'TAGS')
+    '../../allele_components/tags.toml', genotype_g, 'TAG')
 
 print(genotype_t)
 genotype_m = replace_allele_features(
-    '../../allele_components/markers.toml', genotype_t, 'MARKERS')
+    '../../allele_components/markers.toml', genotype_t, 'MARKER')
 
 print(genotype_m)
+
+# %%
