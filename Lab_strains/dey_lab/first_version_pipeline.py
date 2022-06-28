73768e974e4abfe5b66d501e522dc60a5a48612c
Anamika Yadav <anamika310.yadav@gmail.com>
2022-06-28 17:11:43 +0530

    WIP: First version pipeline

--- Lab_strains/dey_lab/first_version_pipeline.py
+++ Lab_strains/dey_lab/first_version_pipeline.py
@@ -1,5 +1,9 @@
 # %%
 import toml
+import pandas as pd
+import json
+
+data = pd.read_csv('strains.tsv', sep='\t')
 
 
 def feature_dict(toml_file):
@@ -37,22 +41,19 @@ def replace_allele_features(toml_file, genotypes, replace_word):
     return genotype_features_replaced
 
 
-genotype = ['cls1-36 ase1-GFP:KanMx6', 'SPAC1002.01']
-genotype_a = replace_allele_features(
-    '../../data/alleles.toml', genotype, 'ALLELE')
-print(genotype_a)
-genotype_g = replace_allele_features(
-    '../../data/gene_IDs.toml', genotype_a, 'GENE')
-print(genotype_g)
+data['Genotype'] = data['Genotype'].astype(str)
+genotypes_allele_replaced = replace_allele_features(
+    '../../data/alleles.toml', data['Genotype'], 'ALLELE')
+genotypes_gene_replaced = replace_allele_features(
+    '../../data/gene_IDs.toml', genotypes_allele_replaced, 'GENE')
+genotypes_tag_replaced = replace_allele_features(
+    '../../allele_components/tags.toml', genotypes_gene_replaced, 'TAG')
 
+genotypes_marker_replaced = replace_allele_features(
+    '../../allele_components/markers.toml', genotypes_tag_replaced, 'MARKER')
+
+with open('first_version_pipeline.txt', 'w', encoding='utf-8') as out:
+    for replaced_genotype in genotypes_marker_replaced:
+        out.write(f'{replaced_genotype}\n')
 
 # %%
-genotype_t = replace_allele_features(
-    '../../allele_components/tags.toml', genotype_g, 'TAG')
-
-print(genotype_t)
-genotype_m = replace_allele_features(
-    '../../allele_components/markers.toml', genotype_t, 'MARKER')
-
-print(genotype_m)
-
