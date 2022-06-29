8788da6d7d55f5bfd7805b27bfc7df6f16628a11
Anamika Yadav <anamika310.yadav@gmail.com>
2022-06-29 17:55:19 +0530

    first version pipeline completed

--- Lab_strains/dey_lab/first_version_pipeline.py
+++ Lab_strains/dey_lab/first_version_pipeline.py
@@ -1,49 +1,37 @@
 # %%
-import toml
+from genestorian_module.replace_feature import replace_allele_features
 import pandas as pd
-import json
-
-data = pd.read_csv('strains.tsv', sep='\t')
 
 
-def feature_dict(toml_file):
-    # a dictionary in which the keys are name, synonyms and toml_keys and values are toml_keys
-    synonyms_2toml_key_dict = {}
-    feature_type_dict = toml.load(toml_file)
-    feature_type_name = list(feature_type_dict.keys())[0]
-    # a dictionary in which the keys are toml keys and value is a dictionary of name, ref, sy
-    toml_key_2feature = feature_type_dict[feature_type_name]
-    for feature_key in toml_key_2feature:
-        if 'name' in toml_key_2feature[feature_key]:
-            name = toml_key_2feature[feature_key]['name']
-            synonyms_2toml_key_dict[name] = feature_key
-        if 'synonyms' in toml_key_2feature[feature_key]:
-            synonyms = toml_key_2feature[feature_key]['synonyms']
-            for synonym in synonyms:
-                synonyms_2toml_key_dict[synonym] = feature_key
-        synonyms_2toml_key_dict[feature_key] = feature_key
-    return synonyms_2toml_key_dict
+genotype = ['cls1-36 ase1-GFP:Kan', 'SPAC1002.01']
+genotype_a = replace_allele_features(
+    '../../data/alleles.toml', genotype, 'ALLELE')
+print(genotype_a)
+genotype_g = replace_allele_features('../../data/gene_IDs.toml', genotype_a, 'GENE')
+print(genotype_g)
 
 
-def replace_allele_features(toml_file, genotypes, replace_word):
-    features = feature_dict(toml_file)
-    genotype_features_replaced = []
-    matches = []
-    for genotype in genotypes:
-        for feature in features.keys():
-            if feature.lower() in genotype.lower():
-                matches.append(feature)
-        matches.sort(key=len, reverse=True)
-        for match in matches:
-            genotype = genotype.replace(match, replace_word)
-        genotype_features_replaced.append(genotype)
+genotype_t = replace_allele_features('../../allele_components/tags.toml', genotype_g, 'TAG')
 
-    return genotype_features_replaced
+print(genotype_t)
+genotype_m = replace_allele_features(
+    '../../allele_components/markers.toml', genotype_t, 'MARKER')
+
+print(genotype_m)
+
+
+# %%
+data = pd.read_csv('strains.tsv', sep='\t', na_filter= False)
 
 
 data['Genotype'] = data['Genotype'].astype(str)
+genotype_list = []
+for genotype in data['Genotype']:
+    genotype_list.append(genotype.lower())
+
+
 genotypes_allele_replaced = replace_allele_features(
-    '../../data/alleles.toml', data['Genotype'], 'ALLELE')
+    '../../data/alleles.toml', genotype_list, 'ALLELE')
 genotypes_gene_replaced = replace_allele_features(
     '../../data/gene_IDs.toml', genotypes_allele_replaced, 'GENE')
 genotypes_tag_replaced = replace_allele_features(
--- Lab_strains/dey_lab/format.py
+++ Lab_strains/dey_lab/format.py
@@ -2,7 +2,15 @@
 import pandas as pd
 
 read_file = pd.read_excel('Manu_Strains.xlsx', usecols=[
-                          'Sample Name', 'Genotype'])
-read_file.to_csv('strains.tsv', sep='\t')
+                          'Sample Name', 'Genotype'], na_filter=False)
 
-# %%
+inconsistent_char_list = ['‚àÜ0', 'Œî']
+
+
+for i in range(len(read_file['Genotype'])):
+    for inconsistent_char in inconsistent_char_list:
+        if inconsistent_char in read_file['Genotype'][i]:
+            read_file['Genotype'][i] = read_file['Genotype'][i].replace(
+                inconsistent_char, 'Δ')
+
+read_file.to_csv('strains.tsv', sep='\t')
