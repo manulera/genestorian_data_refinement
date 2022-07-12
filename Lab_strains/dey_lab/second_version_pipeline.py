755932e0f99bd1e109b0b4cf2b5a9b6ed1e98c36
Anamika Yadav <anamika310.yadav@gmail.com>
2022-07-12 19:57:39 +0530

    sorted by coordinates

--- Lab_strains/dey_lab/second_version_pipeline.py
+++ Lab_strains/dey_lab/second_version_pipeline.py
@@ -1,10 +1,9 @@
 # %%
-import toml
 from genestorian_module import read_strains_tsv
 from genestorian_module.replace_feature import build_feature_dict
 import re
 import json
-
+from operator import itemgetter
 
 def build_strain_list(strain_tsv_file):
     data = read_strains_tsv(strain_tsv_file)
@@ -53,7 +52,7 @@ def build_replaced_feature_dict(feature_dict, allele, replace_word):
 
 
 def find_feature_coords(allele, feature):
-    coords = [(i.start(), i.end()) for i in re.finditer(feature, allele)]
+    coords = [(i.start(), i.end()-1) for i in re.finditer(feature, allele)]
     return coords
 
 
@@ -78,8 +77,12 @@ def build_allele_feature_list(allele_names, toml_files):
                     replaced_feature_dict['feature_type'] = feature_name
                     replaced_feature_dict['coords'] = find_feature_coords(
                         allele_dict['name'], replaced_allele_feature)
+                    
                     allele_dict['allele_features'].append(
                         replaced_feature_dict)
+                    allele_features_sorted = sorted(allele_dict['allele_features'],
+                                     key=itemgetter('coords'), reverse=False)
+                    allele_dict['allele_features'] = allele_features_sorted
     return output_list
 
 
