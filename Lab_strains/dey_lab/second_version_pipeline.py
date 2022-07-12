21b9b74a1850adf49506ba7f90d3b31f99448843
Manuel Lera Ramirez <manulera14@gmail.com>
2022-07-12 09:19:13 +0200

    minor changes

--- Lab_strains/dey_lab/second_version_pipeline.py
+++ Lab_strains/dey_lab/second_version_pipeline.py
@@ -47,8 +47,8 @@ def build_replaced_feature_dict(feature_dict, allele, replace_word):
     for match in matches:
         if match in allele:
             allele_features_matched.append(match)
-        allele = allele.replace(match, replace_word)
-    return (allele, allele_features_matched)
+            allele = allele.replace(match, replace_word)
+    return allele, allele_features_matched
 
 
 def build_allele_feature_list(allele_names, toml_files):
@@ -85,8 +85,8 @@ toml_files = [
 
 strain_list = build_strain_list('strains.tsv')
 allele_names = set({})
-for allele in strain_list:
-    allele_names.update(allele['alleles'])
+for strain in strain_list:
+    allele_names.update(strain['alleles'])
 
 alleles_list = build_allele_feature_list(allele_names, toml_files)
 
@@ -94,20 +94,22 @@ with open('alleles.json', 'w') as fp:
     json.dump(alleles_list, fp, indent=3, ensure_ascii=False)
 
 # %%
+
+
 def find_common_pattern(alleles_list):
-    occurances_dict = {}
+    occurences_dict = {}
     for allele_dict in alleles_list:
         pattern = allele_dict['pattern']
         allele_name = allele_dict['name']
-        if pattern in occurances_dict.keys():
-            occurances_dict[pattern].append(allele_name)
+        if pattern in occurences_dict:
+            occurences_dict[pattern].append(allele_name)
         else:
-            occurances_dict[pattern] = [allele_name]
-    return occurances_dict
+            occurences_dict[pattern] = [allele_name]
+    return occurences_dict
 
 
-occurances_dict = find_common_pattern(alleles_list)
-with open('occurances2.json', 'w') as fp:
-    json.dump(occurances_dict, fp, indent=3, ensure_ascii=False)
+occurences_dict = find_common_pattern(alleles_list)
+with open('occurences2.json', 'w') as fp:
+    json.dump(occurences_dict, fp, indent=3, ensure_ascii=False)
 
 # %%
--- genestorian_module/genestorian_module/replace_feature.py
+++ genestorian_module/genestorian_module/replace_feature.py
@@ -10,14 +10,14 @@ def build_feature_dict(toml_file):
     toml_key_2feature = feature_type_dict[feature_type_name]
     for feature_key in toml_key_2feature:
         if 'name' in toml_key_2feature[feature_key]:
-            name = toml_key_2feature[feature_key]['name'] 
+            name = toml_key_2feature[feature_key]['name']
             synonyms_2toml_key_dict[name] = feature_key
         if 'synonyms' in toml_key_2feature[feature_key]:
             synonyms = toml_key_2feature[feature_key]['synonyms']
             for synonym in synonyms:
                 synonyms_2toml_key_dict[synonym] = feature_key
         synonyms_2toml_key_dict[feature_key] = feature_key
-    return (synonyms_2toml_key_dict, feature_type_name.upper())
+    return synonyms_2toml_key_dict, feature_type_name.upper()
 
 
 def replace_allele_features(toml_file, genotypes, replace_word):
@@ -34,7 +34,3 @@ def replace_allele_features(toml_file, genotypes, replace_word):
         genotype_features_replaced.append(genotype)
 
     return genotype_features_replaced
-
-
-
-
