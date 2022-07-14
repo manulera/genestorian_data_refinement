472dbec14772490ebc1ceb1018442d0e43fecccb
Anamika Yadav <anamika310.yadav@gmail.com>
2022-07-14 12:08:51 +0530

    tsv file unidentified occurence

--- Lab_strains/dey_lab/unidentified_allele_features.py
+++ Lab_strains/dey_lab/unidentified_allele_features.py
@@ -1,11 +1,23 @@
-#%%
+# %%
 import json
 import re
+from collections import Counter
+import pandas as pd
 
 with open('alleles.json') as f:
-    allele_feature_list = json.load(f) 
+    allele_feature_list = json.load(f)
+unidentified_allele_feature_list = []
 for allele in allele_feature_list:
     pattern = allele['pattern']
-    strings = re.split(r'ALLELE|GENE|TAG|MARKER|PROMOTER', pattern)
-    print(strings)
+    split_alleles = re.split(r'ALLELE|GENE|TAG|MARKER|PROMOTER', pattern)
+    unidentified_allele_feature_list = (
+        unidentified_allele_feature_list + split_alleles)
+
+df_unidentified_feature_occurences = pd.DataFrame(Counter(unidentified_allele_feature_list).items(), columns=[
+    'feature', 'no_of_occurence'])
+df_unidentified_feature_occurences = df_unidentified_feature_occurences.sort_values(
+                                            'no_of_occurence', ascending=False)
+df_unidentified_feature_occurences.to_csv(
+    'unidentified_allele_feature.tsv', sep='\t', index=False)
+
 # %%
